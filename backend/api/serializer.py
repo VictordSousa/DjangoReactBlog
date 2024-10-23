1109a628a051020b4650734a8636b00bbd990b14
Victor Pereira <pereira.victorsousa@gmail.com>
2024-10-23 07:16:05 -0300

    First commit, contains all the configuration and views from de begin of project

---
+++ backend/api/serializer.py
@@ -0,0 +1,170 @@
+from django.contrib.auth.password_validation import validate_password
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework import serializers
+#from rest_framework.validators import UniqueValidator
+#from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+from api import models as api_models
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    '''
+    class MyTokenObtainPairSerializer(TokenObtainPairSerializer):: This line creates a new token serializer called MyTokenObtainPairSerializer that is based on an existing one called TokenObtainPairSerializer. Think of it as customizing the way tokens work.
+    @classmethod: This line indicates that the following function is a class method, which means it belongs to the class itself and not to an instance (object) of the class.
+    def get_token(cls, user):: This is a function (or method) that gets called when we want to create a token for a user. The user is the person who's trying to access something on the website.
+    token = super().get_token(user): Here, it's asking for a regular token from the original token serializer (the one it's based on). This regular token is like a key to enter the website.
+    token['full_name'] = user.full_name, token['email'] = user.email, token['username'] = user.username: This code is customizing the token by adding extra information to it. For example, it's putting the user's full name, email, and username into the token. These are like special notes attached to the key.
+    return token: Finally, the customized token is given back to the user. Now, when this token is used, it not only lets the user in but also carries their full name, email, and username as extra information, which the website can use as needed.
+    '''
+
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims to the token
+        token['full_name'] = user.full_name
+        token['email'] = user.email
+        token['username'] = user.username
+        return token
+
+
+# Define a serializer for user registration, which inherits from serializers.ModelSerializer
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        # Specify the model that this serializer is associated with
+        models = api_models.User
+
+        fields = ('full_name', 'email', 'password', 'password2')
+
+    def validate (self, attr):
+        if attr['password'] != attr['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        
+        return attr
+    
+    def create (self, validated_data):
+        user = api_models.User.objects.create(
+            full_name = validated_data['full_name'],
+            email = validated_data['email'],
+        )
+
+
+        email_username, mobile = user.email.split('@')
+        user.username = email_username
+
+
+        user.set_password(validate_password['password'])
+        user.save()
+
+        return user
+    
+
+class UserSerializer (serializers.ModelSerializer):
+    class Meta:
+        model = api_models.User
+        fields = "__all__"
+
+
+class ProfileSerializer (serializers.ModelSerializer):
+    class Meta:
+        model = api_models.Profile
+        fields = "__all__"
+
+
+class CategorySerializer (serializers.ModelSerializer):
+    post_count = serializers.SerializerMethodField()
+
+    '''
+        category.post_set: In Django, when you define a ForeignKey relationship from one model to another 
+        (e.g., Post model having a ForeignKey relationship to the Category model), 
+        Django creates a reverse relationship from the related model back to the model that has the ForeignKey. 
+        By default, this reverse relationship is named <model>_set. In this case, since the Post model has a 
+        ForeignKey to the Category model, Django creates a reverse relationship from Category to Post named post_set. 
+        This allows you to access all Post objects related to a Category instance.
+    '''
+    def get_post_count(self, category):
+        return category.posts.count()
+    
+    class Meta:
+        model = api_models.Category
+        fields = [
+            "id",
+            "title",
+            "image",
+            "slug",
+            "post_count",
+        ]
+
+
+class  CommentSerializer (serializers.ModelSerializer):
+
+    class Meta:
+        model = api_models.Comment
+        fields = "__all__"
+
+
+    def __init__(self, *args, **kwargs):
+        super(CommentSerializer, self).__init__(*args, **kwargs)
+        request = self.context.get('request')
+        if request and request.method == 'POST':
+            self.Meta.depth = 0
+        else:
+            self.Meta.depth = 1
+
+
+class  PostSerializer (serializers.ModelSerializer):
+
+    class Meta:
+        model = api_models.Post
+        fields = "__all__"
+
+
+    def __init__(self, *args, **kwargs):
+        super(PostSerializer, self).__init__(*args, **kwargs)
+        request = self.context.get('request')
+        if request and request.method == 'POST':
+            self.Meta.depth = 0
+        else:
+            self.Meta.depth = 1
+
+
+class  BookmarkSerializer (serializers.ModelSerializer):
+
+    class Meta:
+        model = api_models.Bookmark
+        fields = "__all__"
+
+
+    def __init__(self, *args, **kwargs):
+        super(BookmarkSerializer, self).__init__(*args, **kwargs)
+        request = self.context.get('request')
+        if request and request.method == 'POST':
+            self.Meta.depth = 0
+        else:
+            self.Meta.depth = 1
+
+
+class  NotificationSerializer (serializers.ModelSerializer):
+
+    class Meta:
+        model = api_models.Notification
+        fields = "__all__"
+
+
+    def __init__(self, *args, **kwargs):
+        super(NotificationSerializer, self).__init__(*args, **kwargs)
+        request = self.context.get('request')
+        if request and request.method == 'POST':
+            self.Meta.depth = 0
+        else:
+            self.Meta.depth = 1
+
+
+class AuthorSerializer(serializers.Serializer):
+    views = serializers.IntegerField(default=  0)
+    post = serializers.IntegerField(default=  0)
+    likes = serializers.IntegerField(default=  0)
+    bookmarks = serializers.IntegerField(default=  0)
---
+++ backend/api/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.conf import settings
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.db.models import Sum
+# Restframework
+from rest_framework import status
+from rest_framework.decorators import api_view, APIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from datetime import datetime
+
+# Others
+import json
+import random
+
+# Custom Imports
+from api import serializer as api_serializer
+from api import models as api_models
+
+# This code defines a DRF View class called MyTokenObtainPairView, which inherits from TokenObtainPairView.
+class MyTokenObtainPairView(TokenObtainPairView):
+    # Here, it specifies the serializer class to be used with this view.
+    serializer_class = api_serializer.MyTokenObtainPairSerializer
+
+# This code defines another DRF View class called RegisterView, which inherits from generics.CreateAPIView.
+class RegisterView(generics.CreateAPIView):
+    # It sets the queryset for this view to retrieve all User objects.
+    queryset = api_models.User.objects.all()
+    # It specifies that the view allows any user (no authentication required).
+    permission_classes = (AllowAny,)
+    # It sets the serializer class to be used with this view.
+    serializer_class = api_serializer.RegisterSerializer
+
+
+# This code defines another DRF View class called ProfileView, which inherits from generics.RetrieveAPIView and used to show user profile view.
+class ProfileView(generics.RetrieveUpdateAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = api_serializer.ProfileSerializer
+
+    def get_object(self):
+        user_id = self.kwargs['user_id']
+
+        user = api_models.User.objects.get(id=user_id)
+        profile = api_models.Profile.objects.get(user=user)
+        return profile
+    
+
